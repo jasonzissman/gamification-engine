7843107b5808c1ea13c9cbfa5970f0e83518fa8a
Jason Zissman <jason.zissman@gmail.com>
2022-04-08 12:17:22 -0500

    update dependencies

--- src/entity/entity-routes.js
+++ src/entity/entity-routes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get("/:entityIdField/:entityIdValue/progress", async (request, response) => {
     // TODO authorize request - put in common middleware?
+    
     const entity = await entityHelper.getEntityProgress(request.params.entityIdField, request.params.entityIdValue);
     if (entity) {
         response.status(200).send(entity);
